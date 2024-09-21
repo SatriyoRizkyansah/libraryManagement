b78f7a9d27d6adf7e3b294588a9eed938353c55c
Satriyo Rizkyansah <satttrioo@gmail.Com>
2024-09-21 17:05:36 +0700

    implementasion template

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,17 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>  
-    <x-app-layout>
-    
-    </x-app-layout>
-    
-    <h1>User page</h1>
 
-</body>
+  <head>
+
+    @include('home.css')
+
+  </head>
+
+<body>
+
+@include('home.header')
+
+@include('home.main_banner')
+  
+@include('home.category')
+
+@include('home.book')
+
+@include('home.footer')
+
+  </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+route::get('/', [HomeController::class, 'index']);
+
 
 Route::middleware([
     'auth:sanctum',
