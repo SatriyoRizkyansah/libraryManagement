530cfe6d49b57ab64114e1c2c745c01324a7096d
Satriyo Rizkyansah <satttrioo@gmail.Com>
2024-09-25 10:15:37 +0700

    add a category data

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
@@ -21,5 +22,19 @@ class AdminController extends Controller
         } else { 
             return redirect()->back();
         }
+    }
+
+    public function category_page(){
+        return view('admin.category');
+    }
+
+    public function add_category(Request $request){
+        $data = new Category;
+
+        $data->cat_title = $request->category;
+
+        $data->save();
+
+        return redirect()->back();
     }
 }
---
+++ resources/views/admin/category.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+  <head> 
+    @include('admin.css')
+
+    <style type="text/css">
+        .div_center{
+            text-align: center;
+            margin: auto;
+        }
+
+        .cat_label
+        {
+            font-size: 30px;
+            font-weight: bold;
+            padding: 30px;
+            color: white;
+        }
+    </style>
+
+  <body>
+    @include('admin.header')
+    <div class="d-flex align-items-stretch">
+      <!-- Sidebar Navigation-->
+    @include('admin.sidebar ')
+      <!-- Sidebar Navigation end-->
+    
+       <div class="page-content">
+        <div class="page-header">
+          <div class="container-fluid">
+
+            <div class="div_center">
+
+                <h1 class="cat_label">Add Category</h1>
+
+                <form action="{{ url('add_category') }}" method="Post">
+                    @csrf
+
+                    <label for="">Category Name</label>
+                    <input type="text" placeholder="Enter Category Name" name="category">
+                    
+                    <input class="btn btn-primary ml-2" type="submit" value="Add Category">
+
+                </form>
+
+            </div>
+
+          </div>
+        </div>
+       </div>
+    
+    @include('admin.footer')
+  </body>
+</html>
